b60a842e910e958b7160956e226c685a181ed83c
John Wiseman <jwiseman@morseproject.com>
2013-11-07 16:52:24 -0800

    Fixed github URL.  Added license to project.clj.

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject com.lemonodor/getopt "0.1.0-SNAPSHOT"
   :description "GNU getopt for clojure."
-  :url "http://github.com/wiseman/getopt/"
+  :url "http://github.com/wiseman/clj-getopt/"
+  :license {:name "Eclipse Public License"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
