79eac70366993e3b95cea6b2f1d424ad310c3052
John Wiseman <jwiseman@morseproject.com>
2013-11-07 17:08:19 -0800

    Fixed license in project.clj.  Updated version to 0.1.0.

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
-(defproject com.lemonodor/getopt "0.1.0-SNAPSHOT"
+(defproject com.lemonodor/getopt "0.1.0"
   :description "GNU getopt for clojure."
   :url "http://github.com/wiseman/clj-getopt/"
-  :license {:name "Eclipse Public License"}
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
