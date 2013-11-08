3b5058df4dd5381063829bdb206bbadd72d8f55f
John Wiseman <jwiseman@morseproject.com>
2013-11-07 17:23:51 -0800

    Fixed typo.

--- project.clj
+++ project.clj
@@ -4,5 +4,5 @@
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :scm {:name "git"
-        :url "https://github.com/wiseman/clj-getopt"})
+        :url "https://github.com/wiseman/clj-getopt"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
