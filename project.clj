32c7c0561ab8122cd4a506402236c3acd5ec9012
John Wiseman <jwiseman@morseproject.com>
2013-11-07 17:23:00 -0800

    Added :scm to project.clj.

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://github.com/wiseman/clj-getopt/"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
+  :scm {:name "git"
+        :url "https://github.com/wiseman/clj-getopt"})
   :dependencies [[org.clojure/clojure "1.5.1"]])
