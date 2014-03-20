4cbb5d0aa88a3ed967ff9018c8a94e9be3f8d2f9
John Wiseman <jwiseman@morseproject.com>
2014-03-19 20:28:05 -0700

    Added automatic test coverage check.

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://www.opensource.org/licenses/mit-license.php"}
   :scm {:name "git"
         :url "https://github.com/wiseman/clj-getopt"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:plugins [[lein-cloverage "1.0.2"]]}})
